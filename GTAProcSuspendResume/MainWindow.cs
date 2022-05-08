8073f57fffb6fda2eeb2200e4d5491e4cf8bba3f
Jugius <jugius@hotmail.com>
2022-05-09 01:19:44 +0300

    Добавить хукер хоткеев и повесить паузу/продолжение процесса на Ctrl+Alt+F12

--- GTAProcSuspendResume/MainWindow.cs
+++ GTAProcSuspendResume/MainWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GTAProcSuspendResume.Hotkeys;
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -6,9 +7,17 @@ namespace GTAProcSuspendResume
 {
     public partial class MainWindow : Form
     {
+        private bool IsProcessSuspended = false;
+        KeyboardHook hook = new KeyboardHook();
         public MainWindow()
         {
             InitializeComponent();
+            // register the event that is fired after the key press.
+            hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            // register the control + alt + F12 combination as hot key.
+            hook.RegisterHotKey(Hotkeys.ModifierKeys.Control | Hotkeys.ModifierKeys.Alt,
+                Keys.F12);
         }
 
         private void btnSuspend_Click(object sender, EventArgs e)
@@ -17,6 +26,7 @@ namespace GTAProcSuspendResume
             {
                 var proc = GetProcess(txtProcessName.Text);
                 proc.Suspend();
+                IsProcessSuspended = true;
             }
             catch (Exception ex)
             {
@@ -29,11 +39,33 @@ namespace GTAProcSuspendResume
             {
                 var proc = GetProcess(txtProcessName.Text);
                 proc.Resume();
+                IsProcessSuspended = false;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Ошибка возобновления", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            try
+            {
+                if (IsProcessSuspended)
+                {
+                    btnResume_Click(null, null);
+                }
+                else
+                {
+                    btnSuspend_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка возобновления", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // show the keys pressed in a label.
+            //label1.Text = e.Modifier.ToString() + " + " + e.Key.ToString();
         }
 
         private Process GetProcess(string name)
--- GTAProcSuspendResume/Properties/AssemblyInfo.cs
+++ GTAProcSuspendResume/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GTAProcSuspendResume")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("© Andrey Slusarev  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
