0a3746ff71a549c946c3d0dc9c930c704ed79964
Andrew Slusarev <jugius@gmail.com>
2025-03-11 16:15:05 +0200

    Shorten

--- ProcSuspendResume/Hotkeys/KeyPressedEventArgs.cs
+++ ProcSuspendResume/Hotkeys/KeyPressedEventArgs.cs
@@ -6,25 +6,15 @@ namespace ProcSuspendResume.Hotkeys
     /// <summary>
     /// Event Args for the event that is fired after the hot key has been pressed.
     /// </summary>
-    public class KeyPressedEventArgs : EventArgs
-    {
-        private ModifierKeys _modifier;
-        private Keys _key;
+    public class KeyPressedEventArgs : EventArgs, IHotkey
+    { 
+        public ModifierKeys Modifier { get; }
+        public Keys Key { get; }
 
         internal KeyPressedEventArgs(ModifierKeys modifier, Keys key)
         {
-            _modifier = modifier;
-            _key = key;
-        }
-
-        public ModifierKeys Modifier
-        {
-            get { return _modifier; }
-        }
-
-        public Keys Key
-        {
-            get { return _key; }
+            Modifier = modifier;
+            Key = key;
         }
     }
 }
